15b0157af32336cd1832092e22ec7a3d1644256d
Rodrigo <rodrigo.m.castilho@outlook.com>
2020-10-17 14:50:49 -0300

    Connecting reducers in rootReducer

--- 1_frontend/src/Redux/Reducers/RootReducer.js
+++ 1_frontend/src/Redux/Reducers/RootReducer.js
@@ -2,7 +2,7 @@ import {combineReducers} from "redux";
 import scoringReducer from './ScoringReducer'
 
 const rootReducer = combineReducers({
-
+    teamScoring: scoringReducer
 })
 
 export default rootReducer
